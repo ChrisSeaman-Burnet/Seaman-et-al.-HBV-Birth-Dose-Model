30860237c89cfb199d3909e46c13eb7d160e7b5b
ChrisSeaman-Burnet <54095253+ChrisSeaman-Burnet@users.noreply.github.com>
2020-06-04 13:33:00 +1000

    Update importdata.py

--- code/importdata.py
+++ code/importdata.py
@@ -6,7 +6,7 @@ def data_import (working_dir, region, runs):
     regional/country level data and uncertainity bounds"""
     
     import pandas as pd  
-    xls=pd.ExcelFile("hbvbd_model_inputs_revision.xlsx")        #updated 23/03/20
+    xls=pd.ExcelFile("hbvbd_model_inputs_revision.xlsx")        
   
     if region==1:
         level="regions"
@@ -14,13 +14,13 @@ def data_import (working_dir, region, runs):
         level="countries"
     
     if runs == 1:
-        globz=pd.read_excel(xls, "global", index_col=None).iloc[0:30, 1:3]      #eAg is no longer a global variable for sensitivity analysis
+        globz=pd.read_excel(xls, "global", index_col=None).iloc[0:30, 1:3]      
         hbv_pars=pd.DataFrame(pd.read_excel(xls, level))
         return globz, hbv_pars
     elif runs >1: 
        globz=pd.read_excel(xls, "global", index_col=None).iloc[0:30, 1:5]
        hbv_pars=pd.DataFrame(pd.read_excel(xls, level))
-       hbv_pars_lb=pd.DataFrame(pd.read_excel(xls, level+"_lb"))                #hbv_pars for countries is now limited to LMICs, and is longer by ~5 columns
+       hbv_pars_lb=pd.DataFrame(pd.read_excel(xls, level+"_lb"))             
        hbv_pars_ub=pd.DataFrame(pd.read_excel(xls, level+"_ub"))
        
        
